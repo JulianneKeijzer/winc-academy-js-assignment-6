22d7156d0086377527f07138e9feb05f7562ba47
Julianne Keijzer <91735630+JulianneKeijzer@users.noreply.github.com>
2022-03-08 14:43:44 +0100

    Add files via upload

---
+++ script.js
@@ -0,0 +1,17 @@
+const makePizza = function () {
+    console.log("Make the pizza dough and shape it in a figure you like");
+    console.log("Top the pizza with your favorite toppings");
+    console.log("Bake the pizza in the oven until it's ready");
+};
+
+makePizza();
+
+const makeSushi = function () {
+    console.log("Boil the sushi rice");
+    console.log("Cut avocado, cucumber and fish");
+    console.log("Roll the sushi and cut them into rounds");
+};
+
+makeSushi();
+makeSushi();
+
